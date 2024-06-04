cea96bfb59bc130be0e50dee368efa83ae05b0a6
Ludovic Morin <LudovicMorin357@hotmail.com>
2024-06-04 03:08:22 +0000

    feat: Improve scrolling behavior and positioning of intro text

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 window.addEventListener('scroll', function() {
-    const maintext = document.querySelector('#intro_text');
+    const maintext = document.querySelector('#intro_text .left');
 
-    if (window.scrollY > 200) {
+    if (window.scrollY > 172) {
+        // Let the text scroll normally
         maintext.style.position = 'relative';
+        maintext.style.top = '71.5%';
     } else {
+        // Move the text to the center of the viewport
         maintext.style.position = 'fixed';
+        maintext.style.top = '50%';
+        maintext.style.left = '50%';
+        maintext.style.transform = 'translate(-50%, -50%)';
     }
 });
