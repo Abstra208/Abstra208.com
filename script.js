53a2171ad32788890fba3f2fe8ad6dd92ed98654
Ludovic Morin <LudovicMorin357@hotmail.com>
2024-06-30 05:18:48 +0000

    feat: Improve scrolling behavior and navigation styling

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 window.addEventListener('scroll', function() {
-    const maintext = document.querySelector('#intro_text .left');
+    const navigation = document.querySelector('nav');
+    const navigationLeft = document.querySelector('nav .left');
+    const navigationRight = document.querySelector('nav .right');
 
-    if (window.scrollY > 172) {
-        // Let the text scroll normally
-        maintext.style.position = 'relative';
-        maintext.style.top = '71.5%';
-    } else {
-        // Move the text to the center of the viewport
-        maintext.style.position = 'fixed';
-        maintext.style.top = '50%';
-        maintext.style.left = '50%';
-        maintext.style.transform = 'translate(-50%, -50%)';
-    }
+    const maxSize = 45; // Starting size for padding
+    const minSize = 15; // Minimum size for padding
+    const triggerScroll = 100; // Scroll distance to reach min padding
+    const scrollRange = triggerScroll; // Define scrollRange for background color transition
+    const ColorY = Math.min(window.scrollY, triggerScroll); // Limit scrollY to the maximum of the scroll range
+    const opacity = ColorY / scrollRange;
+    const backgroundColor = `rgba(0, 0, 0, ${opacity})`;
+
+    // Calculate the current padding based on scroll position, ensuring it does not go below minSize
+    let currentPadding = maxSize - ((maxSize - minSize) * (window.scrollY / triggerScroll));
+    currentPadding = Math.max(currentPadding, minSize); // Ensure padding does not go below minSize
+
+    // Apply the calculated padding to both left and right
+    navigationLeft.style.padding = currentPadding + 'px';
+    navigationRight.style.padding = currentPadding + 'px';
+    navigation.style.backgroundColor = backgroundColor;
 });
